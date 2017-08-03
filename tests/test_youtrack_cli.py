9fa871c236220473ec93c81c9108f546c02a8cde
Ali-Akber Saifee <ali@indydevs.org>
2017-08-03 08:33:40 +0800

    Remove useless setup/teardown

--- tests/test_youtrack_cli.py
+++ tests/test_youtrack_cli.py
@@ -12,19 +12,11 @@ from youtrack_cli import cli
 
 
 class TestYoutrack_cli(unittest.TestCase):
-    """Tests for `youtrack_cli` package."""
 
-    def setUp(self):
-        """Set up test fixtures, if any."""
-
-    def tearDown(self):
-        """Tear down test fixtures, if any."""
-
-    def test_000_something(self):
-        """Test something."""
+    def test_improt(self):
+        import youtrack_cli
 
     def test_command_line_interface(self):
-        """Test the CLI."""
         runner = CliRunner()
         result = runner.invoke(cli.main)
         assert result.exit_code == 0
