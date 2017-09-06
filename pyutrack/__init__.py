5d571dc558bca93ae62dc330b0aecb1afc950e91
Ali-Akber Saifee <ali@indydevs.org>
2017-09-06 15:31:49 +0800

    Fixup versioneer

--- pyutrack/__init__.py
+++ pyutrack/__init__.py
@@ -6,3 +6,7 @@ __email__ = 'ali@indydevs.org'
 
 from pyutrack.connection import Connection, Credentials
 from pyutrack.resources import *
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
