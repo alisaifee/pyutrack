01a70f9015dc147157302bcd58de76082acfb5c0
Ali-Akber Saifee <ali@indydevs.org>
2017-09-06 15:18:25 +0800

    Add versioneer

--- pyutrack/__init__.py
+++ pyutrack/__init__.py
@@ -3,7 +3,6 @@
 
 __author__ = """Ali-Akber Saifee"""
 __email__ = 'ali@indydevs.org'
-__version__ = '0.1.0'
 
 from pyutrack.connection import Connection, Credentials
 from pyutrack.resources import *
